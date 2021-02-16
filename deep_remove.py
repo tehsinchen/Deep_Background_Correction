2fa46da40aaa881b4f3f1a02881ad25747844ef9
tehsinchen <52068894+tehsinchen@users.noreply.github.com>
2021-02-16 10:17:20 +0800

    Add files via upload

---
+++ deep_remove.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import tifffile
+import numpy as np
+from pycromanager import Bridge
+from pipython import GCSDevice
+from matplotlib import pyplot as plt
+
+
+def deep_remove():
+    model_path = r'C:\Users\PC1\Desktop\python_GUI\model\unet_background.hdf5'
+    model = tf.keras.models.load_model(model_path)
+    with GCSDevice() as e518:
+        e518.ConnectUSB(serialnum='117022217')
+        e518.ONL(list(range(1, 4)), [False] * 3)
+    bridge = Bridge()
+    core = bridge.get_core()
+    try:
+        while True:
+            core.snap_image()
+            tagged_image = core.get_tagged_image()
+            image = np.reshape(tagged_image.pix, newshape=[tagged_image.tags['Height'], tagged_image.tags['Width']])
+            bg = model.predict((image[np.newaxis, :, :, np.newaxis])/65535)
+            result_img = (image - (bg[0, :, :, 0]*65535))
+            plt.imshow(result_img, cmap='gray', vmin=int(np.amin(result_img)), vmax=int(np.amax(result_img)))
+            ax = plt.gca()
+            ax.axes.xaxis.set_ticks([])
+            ax.axes.yaxis.set_ticks([])
+            plt.tight_layout()
+            plt.pause(0.1)
+            plt.cla()
+    except:
+        pass
+
+
+if __name__ == '__main__':
+    deep_remove()
